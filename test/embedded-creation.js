ba7455d3469deee6381c14349f06351c09c1efd8
Andres Serrano <andres.srg@gmail.com>
2015-10-15 11:02:45 -0500

    test init

---
+++ test/embedded-creation.js
@@ -0,0 +1,47 @@
+var expect = require('chai').expect;
+var request = require('supertest-as-promised');
+
+
+describe('Embedded creation', function() {
+  var app;
+
+  before(function() {
+    app = require('../server/server');
+  });
+
+
+  it('creates books in paralell', function() {
+    var library;
+    return request(app)
+      .post('/api/libraries')
+      .send({ name: 'My library' })
+      .expect(200)
+
+      .then(function(res) {
+        library = res.body;
+
+        var booksPromises = [
+            'My first book',
+            'My second book',
+            'My third book'
+          ].map(function(bookName) {
+            return request(app)
+              .post('/api/libraries/' + library.id + '/books')
+              .send({ name: bookName })
+              .expect(200)
+              .then(function(res) { return res.body; });
+          });
+
+        return Promise.all(booksPromises);
+      })
+      .then(function() {
+        return app.models.Library.findById(library.id)
+          .then(function(fetchedLibrary) {
+            var libraryJSON = fetchedLibrary.toJSON();
+            
+            expect(libraryJSON._books.length).to.be.equal(3);
+          });
+      });
+  });
+
+});
